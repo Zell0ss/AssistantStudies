cab7b1f8fbf59ffaafebbeb5d3a5a18f9843cb20
Zell0ss <zelloss@gmail.com>
2024-01-13 21:42:04 +0100

    initial commit

---
+++ chatgpt_voice original.py
@@ -0,0 +1,103 @@
+#%%
+import os
+import openai
+from dotenv import load_dotenv
+import time
+import speech_recognition as sr
+import pyttsx3
+import numpy as np
+from gtts import gTTS
+import subprocess
+#%%
+import yaml
+
+# Intenta cargar el archivo config.yaml desde el mismo directorio
+try:
+    with open("config.yaml", 'r') as archivo_config:
+        config = yaml.safe_load(archivo_config)
+except FileNotFoundError:
+    print("El archivo config.yaml no se encuentra en el directorio.")
+except yaml.YAMLError as e:
+    print(f"Error al cargar el archivo config.yaml: {e}")
+   
+
+#%%
+
+mytext = 'Welcome to me'
+language = 'en'
+# from os.path import join, dirname
+# import matplotlib.pyplot as plt
+# ^ matplotlib is great for visualising data and for testing purposes but usually not needed for production
+openai.api_key=config['openai_apikey']
+load_dotenv()
+model = 'gpt-3.5-turbo'
+# Set up the speech recognition and text-to-speech engines
+r = sr.Recognizer()
+engine = pyttsx3.init("dummy")
+voice = engine.getProperty('voices')[1]
+engine.setProperty('voice', voice.id)
+name = "Josem"
+greetings = [f"whats up master {name}",
+             "yeah?",
+             "Yes, master Josem?",
+             f"Captain on the bridge!"]
+
+# Listen for the wake word "hey pos"
+def listen_for_wake_word(source):
+    print("Listening for 'Hey'...")
+
+    while True:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            if "Jarvis" in text.lower():
+                print("Wake word detected.")
+                engine.say(np.random.choice(greetings))
+                engine.runAndWait()
+                listen_and_respond(source)
+                break
+        except sr.UnknownValueError:
+            pass
+# Listen for input and respond with OpenAI API
+def listen_and_respond(source):
+    print("Listening...")
+
+    while True:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print(f"You said: {text}")
+            if not text:
+                continue
+
+            # Send input to OpenAI API
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}])
+            response_text = response.choices[0].message.content
+            print(response_text)
+            #myobj = gTTS(text = response_text, lang = language, slow = False)
+            #myobj.save("test.wav")
+            #os.system("aplay test.wav")
+            # Speak the response
+            print("speaking")
+            os.system("espeak ' "+response_text + "'")
+            engine.say(response_text)
+            engine.runAndWait()
+
+            if not audio:
+                listen_for_wake_word(source)
+        except sr.UnknownValueError:
+            time.sleep(2)
+            print("Silence found, shutting up, listening...")
+            listen_for_wake_word(source)
+            break
+
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+            engine.say(f"Could not request results; {e}")
+            engine.runAndWait()
+            listen_for_wake_word(source)
+            break
+
+# Use the default microphone as the audio source
+with sr.Microphone() as source:
+    listen_for_wake_word(source)
---
+++ libraries.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt install python3-pyaudio flac python3-espeak espeak python3-dotenv portaudio19-dev
+# sudo apt install libportaudio0 
+sudo apt install libportaudio2 libportaudiocpp0 portaudio19-dev libasound2-plugins --yes 
+
+arecord -l
+# **** List of CAPTURE Hardware Devices ****
+# card 3: Device [USB PnP Sound Device], device 0: USB Audio [USB Audio]
+#   Subdevices: 1/1
+#   Subdevice #0: subdevice #0
+
+aplay -l
+# **** List of PLAYBACK Hardware Devices ****
+# card 0: Headphones [bcm2835 Headphones], device 0: bcm2835 Headphones [bcm2835 Headphones]
+#   Subdevices: 8/8
+# card 4: UACDemoV10 [UACDemoV1.0], device 0: USB Audio [USB Audio]
+#   Subdevices: 1/1
+#   Subdevice #0: subdevice #0
